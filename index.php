7072110fd5d5c3841912ffa3f5830f3b37398283
alep-2000 <alessiopiras056@gmail.com>
2023-07-03 14:50:41 +0200

    Snack 1 completed

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
 <?php
+    // ARRAY MATCHES
+    $mathches = [
+        [
+            'teams' => 'Crotone - Salernitana',
+            'results' => '50-80'
+        ],
+        [
+            'teams' => 'Genova - Oristano',
+            'results' => '20-50'
+        ],
+        [
+            'teams' => 'Monte Rotondo - Roma',
+            'results' => '80-102'
+        ],
+        [
+            'teams' => 'Samassi - Firenze',
+            'results' => '30-30'
+        ],
+        [
+            'teams' => 'Colle Monfortani - Ostia',
+            'results' => '35-90'
+        ],
+        [
+            'teams' => 'Lunghezza - Bolzano',
+            'results' => '50-75'
+        ],
+    ]
 ?>
 
 <!DOCTYPE html>
@@ -9,6 +36,15 @@
     <title>PHP Snacks Blocco 1</title>
 </head>
 <body>
-    
+    <h2>Matches</h2>
+    <!-- LIST RESULTS -->
+    <ul>
+        <?php 
+            // CICLE FOR-EACH
+            foreach($mathches as $match){
+                echo "<li>" .$match['teams']." | " .$match['results'] ."</li>";
+            } 
+        ?>
+    </ul>
 </body>
 </html>
---
+++ readme.md
@@ -0,0 +1,8 @@
+SNACK 1
+PROBLEMA: Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55-60
+
+SOLUZIONE:
+1 - Creare array con le squadre e i il punteggio;
+2 - Creare ciclo for;
+3 - Stampare a schermo l'array;
