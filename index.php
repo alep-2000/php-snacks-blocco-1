6b0e4389a9def1bc32080578f7b8e969d4474cc4
alep-2000 <alessiopiras056@gmail.com>
2023-07-04 17:45:29 +0200

    [F][L]Snack 2 finisched

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+    // SNACK-1
     // ARRAY MATCHES
     $mathches = [
         [
@@ -37,7 +38,26 @@
             'homePoints' => '50',
             'guestPoints' => '75'
         ],
-    ]
+    ];
+    // SNACK-2 
+    // VARIABLES
+    $cName = false;
+    $cMail = false;
+    $cAge = false;
+
+    if(isset($_GET['name']) && isset($_GET['mail']) && isset($_GET['age'])){
+        if(strlen($_GET['name'] > 3)){
+            $cName = true;
+        }
+
+        if(str_contains($_GET['mail'], '.')){
+            $cMail = true;
+        }
+
+        if(is_numeric($_GET['age'])){
+            $cAge = true;
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -46,17 +66,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Snacks Blocco 1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
-    <h2>Matches</h2>
-    <!-- LIST RESULTS -->
-    <ul>
-        <?php 
-            // CICLE FOR-EACH
-            foreach($mathches as $match){
-                echo "<li>" .$match['homeTeam']." - ".$match['guestTeam']." | " .$match['homePoints']." - ".$match['guestPoints'] ."</li>";
-            } 
-        ?>
-    </ul>
+    <h1>SNACK 1</h1>
+    <div>
+        <h2>Matches</h2>
+        <!-- LIST RESULTS -->
+        <ul>
+            <?php 
+                // CICLE FOR-EACH
+                foreach($mathches as $match){
+                    echo "<li>" .$match['homeTeam']." - ".$match['guestTeam']." | " .$match['homePoints']." - ".$match['guestPoints'] ."</li>";
+                } 
+            ?>
+        </ul>
+    </div>
+    <h1>SNACK 2</h1>
+    <div>
+        <h2>Login</h2>
+        <form action="index.php" method="GET">
+            <input type="text" name="name" placeholder="Your name">
+            <input type="email" name="mail" placeholder="Your mail">
+            <input type="number" name="age" placeholder="Your age">
+            <button type="submit">Invia</button>
+        </form>
+    </div>
+    <div class="mt-4">
+        <?php if(isset($_GET['name']) && isset($_GET['mail']) && isset($_GET['age'])){ ?>
+            <?php if($cName && $cMail && $cAge){
+                echo "Accesso Riuscito";
+            }
+            else{
+                echo "Accesso Negato";
+            } ?>    
+        <?php } ?>
+    </div>
 </body>
 </html>
--- readme.md
+++ readme.md
@@ -6,3 +6,30 @@ SOLUZIONE:
 1 - Creare array con le squadre e i il punteggio;
 2 - Creare ciclo for;
 3 - Stampare a schermo l'array;
+
+SNACK 2 
+PROBLEMA: Con un form passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
+SOLUZIONE:
+1 - Creare un form con 3 input a cui passiamo le variabili name, mail e age;
+2 - Dichiarare le tre variabili;
+3 - Fare un controllo;
+    3.1 - ? SE name è più lungo di 3 caratteri setto a true;
+    3.2 - : ALTRIMENTI false;
+4 - Fare un secondo controllo;
+    4.1 - ? SE mail contiene un punto e una chicciola setto a true;
+    4.2 - : ALTRIMENTI false;
+5 - Fare un terzo controllo;
+    5.1 - ? SE age è un numero setto a true;
+    5.2 - : ALTRIMENTI false;
+6 - Racchiudo i controlli in un altro controllo;
+    6.1 - ? SE tutti i controlli sono true, stampo accesso riuscito;
+    6.2 - : ALTRIMENTI stampo accesso negato;
+
+SNACK 3
+PROBLEMA: Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
+
+SOLUZIONE: 
+1 - Creare un array con le date e ogni data a sua volta avrà dei post;
+2 - Creare un ciclo foreach;
+3 - Stampare a schermo;
